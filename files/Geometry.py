2f0f93eb4cc49a87cc46a9444a75c1e648a6666f
Twistedlink07 <cunruh3760@gmail.com>
2013-11-12 19:50:53 -0500

    Consolidated Geometry

--- files/Geometry.py
+++ files/Geometry.py
@@ -5,6 +5,13 @@ class Circle(object):
         self.x = x
         self.y = y
         self.Radius = Radius
+        
+class Rectangle:
+    def __init__(self,right,top,left,bottom):
+        self.right=right
+        self.top=top
+        self.left=left
+        self.bottom=bottom
 
 class Point(object):
     def __init__(self, x, y):
--- files/RectangleClass.py
+++
@@ -1,6 +0,0 @@
-class Rectangle:
-    def __init__(self,right,top,left,bottom):
-        self.right=right
-        self.top=top
-        self.left=left
-        self.bottom=bottom
